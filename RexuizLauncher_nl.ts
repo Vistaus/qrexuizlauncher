d19c12cca53b1494d79177cd04c45ae1dc25dd54
Heimen Stoffels <vistausss@fastmail.com>
2024-09-02 20:06:25 +0200

    Added Dutch translation

---
+++ RexuizLauncher_nl.ts
@@ -0,0 +1,129 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>DialogSelectInstallLocation</name>
+    <message>
+        <source>Select install location...</source>
+        <translation>Kies een installatielocatie…</translation>
+    </message>
+    <message>
+        <source>Install location:</source>
+        <translation>Installatielocatie:</translation>
+    </message>
+    <message>
+        <source>Select...</source>
+        <translation>Kiezen…</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <source>Select directory</source>
+        <translation>Kies een map</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Kiezen</translation>
+    </message>
+</context>
+<context>
+    <name>Launcher</name>
+    <message>
+        <source>Preparing</source>
+        <translation>Bezig met voorbereiden…</translation>
+    </message>
+    <message>
+        <source>Get update information</source>
+        <translation>Update-informatie ophalen</translation>
+    </message>
+    <message>
+        <source>Repositories unavailable</source>
+        <translation>De pakketbronnen zijn niet beschikbaar</translation>
+    </message>
+    <message>
+        <source>Confirmation</source>
+        <translation>Bevestigen</translation>
+    </message>
+    <message>
+        <source>Update size is </source>
+        <translation>De omvang van de update is </translation>
+    </message>
+    <message>
+        <source>. Install it?</source>
+        <translation>. Wil je de update installeren?</translation>
+    </message>
+    <message>
+        <source>Updating</source>
+        <translation>Bezig met bijwerken…</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Foutmelding</translation>
+    </message>
+    <message>
+        <source>File download failed: </source>
+        <translation>Het bestand kan niet worden gedownload: </translation>
+    </message>
+    <message>
+        <source>Failed load index: </source>
+        <translation>De index kan niet worden geladen: </translation>
+    </message>
+    <message>
+        <source>
+Read error: </source>
+        <translation>
+Leesfout: </translation>
+    </message>
+    <message>
+        <source>Failed parse index: </source>
+        <translation>De index kan niet worden verwerkt: </translation>
+    </message>
+    <message>
+        <source>
+Line: </source>
+        <translation>
+Regel: </translation>
+    </message>
+    <message>
+        <source>Select install folder</source>
+        <translation>Kies een installatiemap</translation>
+    </message>
+    <message>
+        <source>Downloading</source>
+        <translation>Bezig met downloaden…</translation>
+    </message>
+    <message>
+        <source>Running</source>
+        <translation>Bezig met uitvoeren…</translation>
+    </message>
+    <message>
+        <source>Digital signature check failed. Maybe you need update RexuizLauncher.</source>
+        <translation>De handtekeningcontrole is mislukt. Wellicht dient RexuizLauncher te worden bijgewerkt.</translation>
+    </message>
+    <message>
+        <source>Update installed. Run game?</source>
+        <translation>De update is geïnstalleerd. Wil je de game starten?</translation>
+    </message>
+    <message>
+        <source>New version of RexuizLauncher available. Open download page?</source>
+        <translation>Er is een nieuwe versie van RexuizLauncher beschikbaar. Wil je de downloadpagina openen?</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>RexuizLauncher</source>
+        <translation>RexuizLauncher</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Ja</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>Nee</translation>
+    </message>
+</context>
+</TS>
